1d08af592e11ec6500d0b1b3b7852e233cb00262
Justus Geramb <admin@justix.dev>
2024-07-21 22:47:24 +0200

    style: adjust text appearance

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="flex flex-col gap-8">
     <HeroSection
-      class="grid gap-x-24 gap-y-16 *:w-full lg:grid-cols-[4fr_3fr] lg:items-center lg:px-8 lg:pt-8"
+      class="grid gap-x-24 gap-y-8 *:w-full lg:grid-cols-[4fr_3fr] lg:items-center lg:px-8 lg:pt-8"
     >
       <ImageAsset
         src="portrait-mobile.jpeg"
         alt="Justus Geramb"
-        height="448"
+        height="512"
         sizes="271px xs:583px sm:719px md:975px"
         fit="cover"
         :modifiers="{ position: 'top' }"
         loading="eager"
-        class="h-[28rem] object-cover lg:hidden"
+        class="h-[32rem] object-cover lg:hidden"
       />
-      <div class="flex flex-col items-start gap-4 !leading-relaxed max-lg:px-8 max-lg:pb-8 md:text-lg">
-        <h2 class="text-2xl leading-snug lg:text-4xl">
+      <div class="flex flex-col items-start gap-4 !leading-relaxed max-lg:px-8 max-lg:text-sm">
+        <h2 class="font-title text-3xl font-semibold leading-snug tracking-tight md:text-4xl">
           Hi, my name is
           <span class="bg-gradient-to-br from-accent/75 to-accent bg-clip-text text-transparent">Justus</span
           >.
@@ -24,7 +24,7 @@
           I build blazingly fast web applications with a focus on search engine optimization and user
           experience.
         </p>
-        <StyledButton primary class="mt-4 !text-base" @click="navigateTo('/about')">
+        <StyledButton primary class="mt-4 lg:text-base" @click="navigateTo('/about')">
           Learn more about me
         </StyledButton>
       </div>
@@ -36,7 +36,7 @@
         sizes="432px"
         loading="eager"
         preload
-        class="rounded-xl max-lg:hidden"
+        class="rounded-xl shadow-md shadow-primary-950/25 max-lg:hidden"
       />
     </HeroSection>
     <div class="border-y border-primary-200 p-8">
@@ -50,9 +50,3 @@
 <script setup>
 useHeadSEO(true, "Justus Geramb", "Justus Geramb", "Dreamer. Entrepreneur. Developer.", "/");
 </script>
-
-<style scoped lang="scss">
-img {
-  @apply shadow-md shadow-primary-950/25;
-}
-</style>
