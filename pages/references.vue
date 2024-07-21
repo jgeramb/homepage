edf68f45b14ea9322738cf2e47076349b8f90430
Justus Geramb <admin@justix.dev>
2024-07-21 23:08:28 +0200

    feat: move archery to references page

---
+++ pages/references.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex flex-col gap-8 pt-8 *:px-8">
+    <Archery />
+  </div>
+</template>
+
+<script setup>
+useHeadSEO(true, "References • Justus Geramb", "References", "Some examples of my work.", "/references");
+</script>
