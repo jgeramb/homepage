80bccb080f6fe5d578a94842a3334611438af67b
Justus Geramb <admin@justix.dev>
2024-07-23 18:12:24 +0200

    feat: add about hero image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,7 +24,8 @@ export default defineNuxtConfig({
       sm: 640,
       md: 768,
       lg: 1024,
-      xl: 1280
+      xl: 1280,
+      "2xl": 1536
     }
   },
 
--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,24 @@
 <template>
-  <div class="flex flex-col gap-8 pt-8 *:px-8">
-    <HeroSection />
-    <div class="border-t border-t-primary-200 pt-8">
+  <div>
+    <HeroSection class="grid w-full place-items-center">
+      <ImageAsset
+        src="hanseforum.jpeg"
+        alt="Hanseforum"
+        height="768"
+        sizes="320px sm:640px md:768px lg:1024px xl:1280px 2xl:1536px"
+        fit="cover"
+        class="h-[calc(var(--vh,1vh)*100-5rem)] w-full object-cover brightness-50 md:h-[calc(var(--vh,1vh)*100-6.3125rem)]"
+      />
+      <div class="absolute flex flex-col gap-4 px-8 text-center">
+        <h1
+          class="font-title text-4xl !leading-snug tracking-tight text-primary-50 drop-shadow-md lg:text-6xl"
+        >
+          {{ Math.floor((Date.now() - new Date("2017-02-01").getTime()) / (1000 * 60 * 60 * 24 * 365.25)) }}
+          years of experience
+        </h1>
+      </div>
+    </HeroSection>
+    <div class="border-t border-t-primary-200 px-8 pt-8">
       <blockquote>
         <p class="text-[min(5.5vw,3.25rem)] !leading-relaxed *:opacity-50">
           <span>&quot;</span>
