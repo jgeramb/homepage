8820463ffe3706a35b412abdab10f264cf21afe8
Justus Geramb <admin@justix.dev>
2024-07-24 16:12:30 +0200

    feat: replace google icons with saman icon svgs

--- components/Archery.vue
+++ components/Archery.vue
@@ -10,12 +10,25 @@
       draggable="false"
       class="pointer-events-none fixed left-8 top-0 z-50 w-[256px] -translate-y-full"
     />
-    <GoogleIcon
+    <svg
       id="hit"
-      class="invisible absolute z-50 -translate-x-1/2 -translate-y-1/2 rounded-full border border-primary-200 bg-accent p-1 text-primary-50"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      class="invisible absolute z-50 -translate-x-1/2 -translate-y-1/2 rounded-full border border-primary-200 bg-accent fill-primary-50 p-1"
     >
-      close
-    </GoogleIcon>
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M5.29289 5.29289C5.68342 4.90237 6.31658 4.90237 6.70711 5.29289L18.7071 17.2929C19.0976 17.6834 19.0976 18.3166 18.7071 18.7071C18.3166 19.0976 17.6834 19.0976 17.2929 18.7071L5.29289 6.70711C4.90237 6.31658 4.90237 5.68342 5.29289 5.29289Z"
+      />
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M18.7071 5.29289C19.0976 5.68342 19.0976 6.31658 18.7071 6.70711L6.70711 18.7071C6.31658 19.0976 5.68342 19.0976 5.29289 18.7071C4.90237 18.3166 4.90237 17.6834 5.29289 17.2929L17.2929 5.29289C17.6834 4.90237 18.3166 4.90237 18.7071 5.29289Z"
+      />
+    </svg>
     <canvas
       ref="arrowCanvas"
       width="168"
@@ -41,9 +54,36 @@
       <div class="w-full">
         <h1 class="mb-4 font-title text-4xl uppercase leading-snug tracking-tight">Archery</h1>
         <ul v-if="gameActive" class="font-title text-sm uppercase leading-relaxed tracking-tight">
-          <li><GoogleIcon>left_click</GoogleIcon>Press and hold <span class="opacity-50">to charge</span></li>
-          <li><GoogleIcon>drag_click</GoogleIcon>Drag <span class="opacity-50">to aim</span></li>
-          <li><GoogleIcon>mouse</GoogleIcon>Release <span class="opacity-50">to shoot</span></li>
+          <li>
+            <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M6 5C5.44772 5 5 5.44772 5 6V18C5 18.5523 5.44772 19 6 19H10C10.5523 19 11 19.4477 11 20C11 20.5523 10.5523 21 10 21H6C4.34315 21 3 19.6569 3 18V6C3 4.34315 4.34315 3 6 3H18C19.6569 3 21 4.34315 21 6V10C21 10.5523 20.5523 11 20 11C19.4477 11 19 10.5523 19 10V6C19 5.44772 18.5523 5 18 5H6ZM11.2929 11.2929C11.5478 11.038 11.9203 10.94 12.2676 11.0365L21.2676 13.5365C21.6496 13.6426 21.9329 13.9643 21.9897 14.3567C22.0465 14.749 21.8662 15.1379 21.53 15.348L17.7257 17.7257L15.348 21.53C15.1379 21.8662 14.749 22.0465 14.3567 21.9897C13.9643 21.9329 13.6426 21.6496 13.5365 21.2676L11.0365 12.2676C10.94 11.9203 11.038 11.5478 11.2929 11.2929Z"
+              />
+            </svg>
+            Press and hold <span class="opacity-50">to charge</span>
+          </li>
+          <li>
+            <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M11.2929 1.39348C11.6834 1.00295 12.3166 1.00295 12.7071 1.39348L16.2426 4.92901C16.6332 5.31954 16.6332 5.9527 16.2426 6.34323L12.7071 9.87876C12.3166 10.2693 11.6834 10.2693 11.2929 9.87876L7.75737 6.34323C7.36684 5.9527 7.36684 5.31954 7.75737 4.92901L11.2929 1.39348ZM5.81282 7.28778C6.07804 7.28778 6.33239 7.39314 6.51993 7.58067L10.2322 11.293C10.4198 11.4805 10.5251 11.7349 10.5251 12.0001C10.5251 12.2653 10.4198 12.5197 10.2322 12.7072L6.51993 16.4195C6.1294 16.81 5.49624 16.81 5.10571 16.4195L1.3934 12.7072C1.20587 12.5197 1.10051 12.2653 1.10051 12.0001C1.10051 11.7349 1.20587 11.4805 1.3934 11.293L5.10571 7.58067C5.29325 7.39314 5.5476 7.28778 5.81282 7.28778ZM17.6569 7.75743C18.0474 7.36691 18.6805 7.36691 19.0711 7.75743L22.6066 11.293C22.9971 11.6835 22.9971 12.3167 22.6066 12.7072L19.0711 16.2427C18.6805 16.6332 18.0474 16.6332 17.6569 16.2427L14.1213 12.7072C13.7308 12.3167 13.7308 11.6835 14.1213 11.293L17.6569 7.75743ZM12 13.4749C12.2652 13.4749 12.5196 13.5803 12.7071 13.7678L16.4194 17.4801C16.8099 17.8706 16.8099 18.5038 16.4194 18.8943L12.7071 22.6066C12.5196 22.7942 12.2652 22.8995 12 22.8995C11.7348 22.8995 11.4804 22.7942 11.2929 22.6066L7.58059 18.8943C7.19007 18.5038 7.19007 17.8706 7.58059 17.4801L11.2929 13.7678C11.4804 13.5803 11.7348 13.4749 12 13.4749Z"
+              />
+            </svg>
+            Drag <span class="opacity-50">to aim</span>
+          </li>
+          <li>
+            <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M21.0711 2.92892C21.3505 3.20836 21.4392 3.62616 21.2973 3.995L14.2262 22.3798C14.0816 22.7559 13.7254 23.0084 13.3226 23.0204C12.9199 23.0323 12.5493 22.8014 12.3825 22.4346L9.00217 14.9978L1.56538 11.6175C1.19857 11.4507 0.967653 11.0801 0.979628 10.6774C0.991604 10.2746 1.24413 9.91839 1.62021 9.77375L20.005 2.70268C20.3738 2.56082 20.7916 2.64948 21.0711 2.92892Z"
+              />
+            </svg>
+            Release <span class="opacity-50">to shoot</span>
+          </li>
         </ul>
       </div>
       <canvas ref="bowCanvas" width="354" height="354" class="touch-none max-lg:mx-auto max-lg:size-64" />
@@ -420,8 +460,8 @@ section {
 ul > li {
   @apply flex items-center gap-2;
 
-  .material-symbols-rounded {
-    @apply text-xl;
+  svg {
+    @apply size-4 fill-primary-950;
   }
 }
 </style>
