55fcb789243c3384a92bfc1883e9da94fcc421c6
Justus Geramb <admin@justix.dev>
2024-07-28 12:12:46 +0200

    fix: min hero section height

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -1,6 +1,6 @@
 <template>
   <section
-    class="min-h-[calc(var(--vh,1vh)*100-5rem-4rem)] md:min-h-[calc(var(--vh,1vh)*100-6.3125rem-4rem)] lg:mx-auto"
+    class="min-h-[calc(var(--vh,1vh)*100-5rem-2rem)] md:min-h-[calc(var(--vh,1vh)*100-6.3125rem-2rem)] lg:mx-auto"
   >
     <slot />
   </section>
