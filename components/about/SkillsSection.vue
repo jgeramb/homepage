adf37e488cecd00fe9035ff45e989aa273e81487
Justus Geramb <admin@justix.dev>
2024-07-30 18:27:36 +0200

    fix: skill order

--- components/about/SkillsSection.vue
+++ components/about/SkillsSection.vue
@@ -20,7 +20,7 @@
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-const skills = await queryContent("skills").find();
+const skills = await queryContent("skills").sort({ rating: -1 }).find();
 
 const wrapper = ref();
 
