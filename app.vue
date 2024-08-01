63c7fcc330e0ddcea662d8301d423a63a3971b52
Justus Geramb <admin@justix.dev>
2024-08-01 13:53:58 +0200

    refactor: remove obsolete scrolltrigger position update

--- app.vue
+++ app.vue
@@ -5,13 +5,7 @@
 </template>
 
 <script setup>
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-
 if (import.meta.client) {
-  gsap.registerPlugin(ScrollTrigger);
-  useTransitionListener(() => setTimeout(() => ScrollTrigger.refresh(), 500));
-
   function updateDocumentHeight() {
     document.documentElement.style.setProperty("--vh", document.documentElement.clientHeight / 100 + "px");
   }
